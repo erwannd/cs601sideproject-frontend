0dd8faba8f92ce2c817f289508a3acc872899824
Andrew Wihardja <andrewadi96@gmail.com>
2024-10-07 16:14:57 -0700

    added connecting to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+  const text = "two dogs";
+
+  useEffect(() => {
+    // localhost:8080/api/search/text?text="two dogs"
+    axios.get(`http://localhost:8080/api/search/text?text=${text}`)
+      .then(response => {
+        setImages(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>Images</h2>
+      <ul>
+        {images.map(img => (
+          <li key={img.id}>
+            {img.title} (id: {img.id}) (url: {img.url}) (score: {img.score})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
