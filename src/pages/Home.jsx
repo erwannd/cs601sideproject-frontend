bce2be30f6fb11f8065b4bdd133b051b55d0f10b
crs0256 <crs0256@gmail.com>
2024-10-10 19:57:03 -0700

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import ImageCollection from './ImageCollection';
+import axios from 'axios';
 
-const Home = () => {
-  return <h1>Welcome</h1>;
+const Home = ({ userId }) => {
+  const [results, setResults] = useState([]);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      if (!userId) return;
+
+      try {
+        const response = await axios.get('http://localhost:8080/api/search/all', {
+          params: { userId: userId },
+        });
+
+        if (response.status === 200) {
+          setResults(response.data);
+          setError('');
+        } else {
+          setError('Failed to query images.');
+        }
+      } catch (error) {
+        console.log(error);
+        setError('An error occurred while querying images.');
+      }
+    };
+
+    fetchImages();
+  }, [userId]);
+
+
+  const renderResults = () => {
+    return <ImageCollection images={results} />;
+  };
+
+  return (
+    <div className="text-search-container">
+      {error && <p className="error-message">{error}</p>}
+      {renderResults()}
+    </div>
+  );
 };
 
 export default Home;
